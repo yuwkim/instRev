dc85a371a1a3203de3a4d2d33708aa21cc5efd80
yuwkim <45239402+yuwkim@users.noreply.github.com>
2018-12-21 00:41:57 -0500

    Update midterm.m

--- midterm.m
+++ midterm.m
@@ -25,50 +25,11 @@ tline=fgetl(fileID);
 %
 if ~contains(file,fileT)
     warning('this is NOT an original data txt file, the consequence of analysis is on you.')
+    warning('Or, if you are using Mac, because of the stupid / and \ issue, you cannot use this sanity check.')
 end
-%% read session information
-% check the linetaker function at the end of the script.
-%
-data1.date=lineTaker(tline,file,'Start',1);
-data1.boxNum=lineTaker(tline,file,'Box:',1);
-data1.programName=lineTaker(tline,file,'MSN:',1);
-data1.totalTrial=str2double(lineTaker(tline,file,'D:',1));
-data1.totalReward=str2double(lineTaker(tline,file,'Q:',1));
-data1.omission=str2double(lineTaker(tline,file,'R:',1));
-data1.totalTimeInSec=str2double(lineTaker(tline,file,'X:',1));
-data1.leftPress=str2double(lineTaker(tline,file,'Y:',1));
-data1.rightPress=str2double(lineTaker(tline,file,'Z:',1));
-%%
-% Let's organize a bit.
-%
-if data1.totalTrial>150 % session ends at >151 trials, so the 151 trial initiated but not completed.
-    data1.totalTrial=150;
-end
-data1.totalTime=join([string(fix(data1.totalTimeInSec/60)) 'min' ...
-    rem(data1.totalTimeInSec,60) 'sec']);
-%% Read numeric data
-% (G: array) making animal choice data (0=omission,1=left,2=right)
-% (J: array) making rewarded levers
-% (L: array) making number of rewardss info
 
-data1.choice=arrayTaker(tline,file,'G:',1);
-data1.lever=arrayTaker(tline,file,'J:',1);
-data1.reward=arrayTaker(tline,file,'L:',1);
-data1.headEntry=arrayTaker(tline,file,'V:',1);
-data1.pressLever=arrayTaker(tline,file,'W:',1);
-
-
-% another sanity check with the same issue when it calculated choice array
-if ~data1.totalReward==sum(data1.reward)
-    warning('this animal hacked the system, be careful with data interpretation')
-end
-% reset indeces
-data1.pctCorrect=sum(data1.reward)/(data1.totalTrial-data1.omission);
-tline=fgetl(fileID);
-
-
-
-
+data=reversalReader(file);
+% wait for it, it takes time, about ~30s?
 
 %% read numeric data
 %
@@ -114,7 +75,7 @@ for j=1:num
         lineName=fgetl(fid);
     end
     nrCul=5; % the Med PC software default value in a result file.
-    totalTrial=150;
+    totalTrial=str2double(lineTaker(lineName,fileName,'D:',num))-1;
     tempArray=nan(round(totalTrial./nrCul)+1,nrCul); % +1, because of 0 trial, software's feature=> every var starts with 0.
     for i=1:length(tempArray)-1
         lineName=fgetl(fid);
@@ -140,4 +101,75 @@ for j=1:num
     output=revisedArray;
     lineName=fgetl(fid);
 end
+fclose(fid);
+end
+
+% reversalReader
+
+function output= reversalReader(fileName)
+fid=fopen(fileName,'rt');
+% frewind(fid);
+tline=fgetl(fid);
+nrAnimals=0;
+nrTotalBoxes=12; % physically the number of behavioral chambers in my lab is 12.
+while nrAnimals<nrTotalBoxes
+    while ~contains(tline,'Box:')
+        tline=fgetl(fid);
+    end
+    tline=fgetl(fid);
+    nrAnimals=nrAnimals+1;
+end
+fclose(fid);
+
+for j=1:nrAnimals
+    %% read session information
+    % check the linetaker function at the end of the script.
+    %
+    data(j).date=lineTaker(tline,fileName,'Start Date:',j);
+    data(j).boxNum=lineTaker(tline,fileName,'Box:',j);
+    data(j).programName=lineTaker(tline,fileName,'MSN:',j);
+    data(j).totalTrial=str2double(lineTaker(tline,fileName,'D:',j));
+    data(j).totalReward=str2double(lineTaker(tline,fileName,'Q:',j));
+    data(j).omission=str2double(lineTaker(tline,fileName,'R:',j));
+    data(j).totalTimeInSec=str2double(lineTaker(tline,fileName,'X:',j));
+    data(j).leftPress=str2double(lineTaker(tline,fileName,'Y:',j));
+    data(j).rightPress=str2double(lineTaker(tline,fileName,'Z:',j));
+    %%
+    % Let's organize a bit.
+    %
+    if data(j).totalTrial>150 % session ends at >j5j trials, so the j5j trial initiated but not completed.
+        data(j).totalTrial=150;
+    end
+    data(j).totalTime=join([string(fix(data(j).totalTimeInSec/60)) 'min' ...
+        rem(data(j).totalTimeInSec,60) 'sec']);
+    %% Read numeric data
+    % (G: array) making animal choice data (0=omission,j=left,2=right)
+    % (J: array) making rewarded levers
+    % (L: array) making number of rewardss info
+    %
+    data(j).choice=arrayTaker(tline,fileName,'G:',j);
+    data(j).lever=arrayTaker(tline,fileName,'J:',j);
+    data(j).reward=arrayTaker(tline,fileName,'L:',j);
+    data(j).headEntry=arrayTaker(tline,fileName,'V:',j);
+    data(j).pressLever=arrayTaker(tline,fileName,'W:',j);
+    
+    
+    % another sanity check with the same issue when it calculated choice array
+    if ~data(j).totalReward==sum(data(j).reward)
+        warning(['animal in the box' data(j).boxNum ' hacked the system, be careful with data interpretation'])
+    end
+    % reset indeces
+    data(j).pctCorrect=sum(data(j).reward)/(data(j).totalTrial-data(j).omission);
+    % nose poke to press lever, because of the medpc coding, a variable
+    % starts with 0, there is 0 trial for head entry (nose poke) and j5jth head
+    % entry to finish the program. In a nutshell, we can get j49 of j50
+    % reaction time. Plus, jitter timing of lever extension applied. jitter
+    % range = 0.js to js.
+    data(j).rtIn10ms=data(j).pressLever(2:150)-data(j).headEntry(1:149);
+    data(j).avgRtInSec=mean(data(j).rtIn10ms(data(j).rtIn10ms>0 & data(j).rtIn10ms<3000))./100;
+    % omission trial has 0 ms reaction time, so if the animal omitted the
+    % trial, the rt will be huge, and it will be screened by indexing them with
+    % <4000.
+end
+ output=data;
 end
