68963902c76e28e08e1621a598763421ff9b7088
yuwkim <45239402+yuwkim@users.noreply.github.com>
2019-01-07 18:44:08 -0500

    Update testFseek.m

--- testFseek.m
+++ testFseek.m
@@ -1,28 +1,26 @@
 clear
 fid = fopen('2018-12-06_14h30m_Subject 20th full RL.txt');
 tline1 = fgetl(fid);
-header='D:';
+header='Box:';
 c=nan(12,1);
 for i=1:12
     while ~contains(tline1,header)
         tline1=fgetl(fid);
         c(i,1)=ftell(fid);
-                %c=ftell(fid);
     end
     tline1=fgetl(fid);
     d=ftell(fid);
 end
-ftell(fid)
-a=diff(c);
+interval=diff(c);
 frewind(fid)
 tline2=fgetl(fid);
 txtFileLineChangingConstant=2; 
 while ~contains(tline2,header)
         tline2=fgetl(fid);
 end
-fseek(fid,a(1,1)-(txtFileLineChangingConstant+length(tline2)),'cof');
-tline3=fgetl(fid);
-fseek(fid,a(1,1)-(txtFileLineChangingConstant+length(tline2)),'cof');
-tline4=fgetl(fid);
-fseek(fid,a(1,1)-(txtFileLineChangingConstant+length(tline2)),'cof');
-tline5=fgetl(fid);
+tline=nan(length(interval),1);
+tline=string(tline);
+for i=1:11
+fseek(fid,min(interval)-(txtFileLineChangingConstant+length(tline2)),'cof');
+tline(i,1)=fgetl(fid);
+end
